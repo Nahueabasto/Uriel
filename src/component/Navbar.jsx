0007d7c6d7b6537dd0ec7df3f8731ed3f4148b65
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-11-26 15:35:27 -0300

    w

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,74 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-//  import MenuIcon from "@mui/icons-material/Menu";
-// import CloseIcon from "@mui/icons-material/Close";
-// import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// // import img from "../img/lo.png";
-// import "./Navbar.css";
-
-// const Navbar = () => {
-//   const [click, setClick] = useState(false);
-//   const handleClick = () => setClick(!click);
-
-//   const [color, setColor] = useState(false);
-//   const changeColor = () => {
-//     if (window.scrollY >= 100) {
-//       setColor(true);
-//     } else {
-//       setColor(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", changeColor);
-//     return () => {
-//       window.removeEventListener("scroll", changeColor);
-//     };
-//   }, []);
-
-//   return (
-// <div className={color ? "header header-bg" : "header"}>
-//       <Link to="/">
-//       <h1 className="portfolio-title">pipa</h1>
-//       </Link>
-
-//       <ul className={click ? "nav-menu active" : "nav-menu"}>
-//         <li>
-//           <Link to="/" onClick={handleClick}>Inicio</Link>
-//         </li>
-//         <li>
-//           <Link to="/projects" onClick={handleClick}>Proyectos</Link>
-//         </li>
-//         <li>
-//           <Link to="/about" onClick={handleClick}>Sobre mi</Link>
-//         </li>
-//       </ul>
-//       <div className="hamburguer" onClick={handleClick}>
-//         {click ? (
-//           <CloseIcon size={30} style={{ color: "#fff" }} />
-//         ) : (
-//           <MenuIcon size={30} style={{ color: "#fff" }} />
-//         )}
-//       </div>
-//       <div className="icons">
-//            <a href="" target="_blank" rel="noopener noreferrer">
-//              <WhatsAppIcon className="mui" size={20} style={{ color: "#fff", fontSize: "large", marginRight: "1rem" }} />
-//            </a>
-//            <a href="" target="_blank" rel="noopener noreferrer">
-//              <FacebookIcon className="mui" size={20} style={{ color: "#fff", fontSize: "large", marginRight: "1rem" }} />
-//            </a>
-//            <a href="" target="_blank" rel="noopener noreferrer">
-//              <InstagramIcon className="mui" size={20} style={{ color: "#fff", fontSize: "large", marginRight: "1rem" }} />
-//            </a>
-       
-//          </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -135,9 +64,17 @@ const Navbar = () => {
 
         {/* Sección de contacto solo visible en el menú hamburguesa */}
         {click && (
+
           <div className="contact-info">
-            <p><EmailIcon style={{ color: "#fff", marginRight: "0.2rem" }} /> @gmail.com</p>
-            <p><WhatsAppIcon style={{ color: "#fff", marginRight: "0.2rem" }} />333333333</p>
+            <div className="seguinos">
+            <p>Seguinos</p>
+            <p> <InstagramIcon style={{ color: "#fff", marginRight: "0.2rem", marginBottom: "10px" }} />  </p>
+            </div>
+            <div className="contactanos">
+            <p>Contactanos</p>
+            <p><EmailIcon style={{ color: "#fff", marginRight: "0.2rem",  }} /> @gmail.com</p>
+            <p><WhatsAppIcon style={{ color: "#fff", marginRight: "0.2rem"}} />333333333</p>
+            </div>
           </div>
         )}
       </ul>
